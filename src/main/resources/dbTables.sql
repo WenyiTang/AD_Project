d8e9c2bc3eb477ad22bebec6c3dceec12fd12d8c
Zhe Rui Tan <32306977+malcolmtzr@users.noreply.github.com>
2022-01-29 21:27:34 +0800

    update sql script

--- src/main/resources/dbTables.sql
+++ src/main/resources/dbTables.sql
@@ -4,7 +4,7 @@ USE foodDiaryApp;
 
 CREATE TABLE foodDiaryApp.Goal (
 	goalid INTEGER NOT NULL AUTO_INCREMENT,
-	goaldescription VARCHAR NULL,
+	goaldescription VARCHAR(250) NULL,
 	totalmealcount INTEGER NULL,
 	targetcount INTEGER NULL,
 	status ENUM('STARTED', 'IN_PROGRESS', 'COMPLETED', 'CANCELLED') NOT NULL,
@@ -25,22 +25,20 @@ CREATE TABLE foodDiaryApp.Comment (
 
 CREATE TABLE foodDiaryApp.MealEntry (
 	mealentryid INTEGER NOT NULL AUTO_INCREMENT,
-	imageurl VARCHAR NULL,
+	imageurl VARCHAR(500) NULL,
 	visibility BOOLEAN NULL,
-	title VARCHAR NULL,
-	description VARCHAR NULL,
+	title VARCHAR(50) NULL,
+	description VARCHAR(250) NULL,
 	flagged BOOLEAN NULL,
-	feelings ENUM('CRYING', 'PENSIVE', 'HAPPY', 'JOYFUL') NULL,
+	feelings ENUM('CRYING', 'PENSIVE', 'HAPPY', 'JOYFUL') NOT NULL,
 	trackscore INTEGER NULL,
 	timestamp DATETIME NULL,
 	goalid INTEGER NOT NULL,
-	likerid INTEGER NULL,
 	authorid INTEGER NOT NULL,
-	commentid INTEGER NULL,
 	PRIMARY KEY (mealentryid),
 	CONSTRAINT goalidfk
 		FOREIGN KEY (goalid) REFERENCES foodDiaryApp.Goal (goalid) ON DELETE NO ACTION ON UPDATE NO ACTION,
 	CONSTRAINT authoridfk
-		FOREIGN KEY (authorid) REFERENCES foodDiaryApp.User (userid) ON DELETE NO ACTION ON UPDATE NO ACTION,
+		FOREIGN KEY (authorid) REFERENCES foodDiaryApp.User (userid) ON DELETE NO ACTION ON UPDATE NO ACTION
 		
 );
