9b83edb9e981ace8f4ed8276713a10555ea2ada6
Zhe Rui Tan <32306977+malcolmtzr@users.noreply.github.com>
2022-01-29 15:22:08 +0800

    rework sql script

--- src/main/resources/dbTables.sql
+++ src/main/resources/dbTables.sql
@@ -1,16 +1,41 @@
+DROP SCHEMA IF EXISTS foodDiaryApp;
+CREATE SCHEMA foodDiaryApp;
+USE foodDiaryApp;
+
+CREATE TABLE foodDiaryApp.Goal (
+	goalid INTEGER NOT NULL AUTO_INCREMENT,
+	goaldescription VARCHAR NULL,
+	totalmealcount INTEGER NULL,
+	targetcount INTEGER NULL,
+	status ENUM('STARTED', 'IN_PROGRESS', 'COMPLETED', 'CANCELLED') NOT NULL,
+	startdate DATE NULL,
+	enddate DATE NULL,
+	PRIMARY KEY (goalid)
+);
+
+CREATE TABLE foodDiaryApp.User (
+	userid INTEGER NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (userid)
+);
+
+CREATE TABLE foodDiaryApp.Comment (
+	commentid INTEGER NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (commentid)
+);
+
 CREATE TABLE foodDiaryApp.MealEntry (
 	mealentryid INTEGER NOT NULL AUTO_INCREMENT,
 	imageurl VARCHAR NULL,
 	visibility BOOLEAN NULL,
 	title VARCHAR NULL,
 	description VARCHAR NULL,
-	flagged VARCHAR NULL,
+	flagged BOOLEAN NULL,
 	feelings ENUM(...) NULL,
 	trackscore INTEGER NULL,
 	timestamp DATETIME NULL,
 	goalid INTEGER NOT NULL,
 	likerid INTEGER NULL,
-	authorid INTEGER NOT NULL
+	authorid INTEGER NOT NULL,
 	commentid INTEGER NULL,
 	PRIMARY KEY (mealentryid),
 	CONSTRAINT goalidfk
@@ -22,16 +47,4 @@ CREATE TABLE foodDiaryApp.MealEntry (
 	CONSTRAINT commentidfk
 		FOREIGN KEY (commentid) REFERENCES foodDiaryApp.Comment (commentid) ON DELETE NO ACTION ON UPDATE NO ACTION
 		
-);
-
-
-CREATE TABLE foodDiaryApp.Goal (
-	goalid INTEGER NOT NULL AUTO_INCREMENT,
-	goaldescription VARCHAR NULL,
-	totalmealcount INTEGER NULL,
-	targetcount INTEGER NULL,
-	status ENUM('STARTED', 'IN_PROGRESS', 'COMPLETED', 'CANCELLED') NOT NULL,
-	startdate DATE NULL,
-	enddate DATE NULL,
-	PRIMARY KEY (goalid)
 );
