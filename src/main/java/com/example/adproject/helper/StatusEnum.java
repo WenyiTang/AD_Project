275e1444480eed05599fa5fda30bcb08ffd25e17
Zhe Rui Tan <32306977+malcolmtzr@users.noreply.github.com>
2022-01-29 10:03:40 +0800

    Completed Goal and added StatusEnum

---
+++ src/main/java/com/example/adproject/helper/StatusEnum.java
@@ -0,0 +1,5 @@
+package com.example.adproject.helper;
+
+public enum StatusEnum {
+	STARTED, IN_PROGRESS, COMPLETED, CANCELLED
+}
--- src/main/java/com/example/adproject/model/Goal.java
+++ src/main/java/com/example/adproject/model/Goal.java
@@ -1,9 +1,13 @@
 package com.example.adproject.model;
 
+import java.time.LocalDate;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import com.example.adproject.helper.StatusEnum;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,5 +20,21 @@ public class Goal {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	private String goalDescription;
+	private int totalMealCount;
+	private int targetCount;
+	private StatusEnum status;
+	private LocalDate startDate;
+	private LocalDate endDate;
+	
+	public Goal(String goalDescription, int totalMealCount, int targetCount,
+				StatusEnum status, LocalDate startDate, LocalDate endDate) {
+		this.goalDescription = goalDescription;
+		this.totalMealCount = totalMealCount;
+		this.targetCount = targetCount;
+		this.status = status;
+		this.startDate = startDate;
+		this.endDate = endDate;
+	}
 	
 }
