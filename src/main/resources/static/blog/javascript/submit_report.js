4eb184477a84d0cf7df7c75d36319c6171238b52
Daniel <danielchua93@gmail.com>
2022-02-07 04:02:20 +0800

    completed javascript function to send form data to ReportAPI to submit new report

--- src/main/resources/static/blog/javascript/submit_report.js
+++ src/main/resources/static/blog/javascript/submit_report.js
@@ -9,14 +9,35 @@ function submitReport() {
     
     let flagForm = document.querySelector("#flag-form");
 
+    let userId = flagForm.getAttribute("data-userId");
+
+    let mealEntryId = flagForm.getAttribute("data-mealEntryId");
+
+    // console.log("userId = " + userId);
+    // console.log("mealEntryId = " + mealEntryId);
+
     // console.log("flagform value = " + flagForm.reason.value);
 
-    // fetch (
-    //     "http://localhost:8080/api/report/flag?" +
-    //     new URLSearchParams({
+    fetch (
+        "http://localhost:8080/api/report/submit?" +
+        new URLSearchParams({
+            userId: userId,
+            mealEntryId: mealEntryId,
+            reason: flagForm.reason.value
+        }), {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        }
+    ).then(
+        report => {
+            window.location.href = "http://localhost:8080/blog/view/entry/" + mealEntryId;
 
-    //     })
-    // )
+            // to view Report persisted to database, uncomment line below
+            console.log(report.json());
+        }
+    );
 
     
 }
