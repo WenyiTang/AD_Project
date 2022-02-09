473a8d131b6f92e9ed4c5832eb1b567d78ae26d9
Zhe Rui Tan <32306977+malcolmtzr@users.noreply.github.com>
2022-02-09 12:53:34 +0800

    Tested request and response to Flask. Needs work on JSON

--- src/main/java/com/example/adproject/controller/MealEntryController.java
+++ src/main/java/com/example/adproject/controller/MealEntryController.java
@@ -5,6 +5,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
+import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -19,7 +20,9 @@ import java.util.List;
 import com.example.adproject.helper.FeelingEnum;
 import com.example.adproject.model.MealEntry;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonArrayFormatVisitor;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -100,7 +103,8 @@ public class MealEntryController {
 			testTrackScore.add(1);
 			testTrackScore.add(1);
 			testTrackScore.add(1);
-			sendDataToFlaskWMA(3, testTrackScore);
+			testsenddata(3, testTrackScore);
+			//sendDataToFlaskWMA(3, testTrackScore);
 			return true;
 		}
 		catch (IOException e) {
@@ -109,20 +113,73 @@ public class MealEntryController {
 		}
 	}
 
+	private void testsenddata(int targetCount, List<Integer> trackScore) {
+		try {
+			//step 1: test connection (Passed)
+			URL testurl = new URL("http://127.0.0.1:5000/suggestnextmeal");
+			HttpURLConnection httpURLConnection = (HttpURLConnection) testurl.openConnection();
+			httpURLConnection.setConnectTimeout(10000);
+			httpURLConnection.setReadTimeout(10000);
+			httpURLConnection.setRequestMethod("POST");
+			httpURLConnection.setRequestProperty("Content-Type", "application/json: charset=UTF-8");
+			httpURLConnection.setRequestProperty("Accept", "application/json");
+			httpURLConnection.setDoOutput(true);
+
+			InputStream status = httpURLConnection.getErrorStream();
+			System.out.println(status);
+
+			//step 3: test if JSON object is being sent (Failed)
+			//Flask not receiving Json data
+			String targetCountString = String.valueOf(targetCount);
+			String trackScoreString = trackScore.toString();
+			System.out.println(trackScoreString);
+			String jsonString = "{\"targetCount\":"+"\""+targetCountString+"\","+"\"trackScore\":"+"\""+trackScoreString+"\"}";
+			System.out.println(jsonString);
+
+			OutputStream ops = httpURLConnection.getOutputStream();
+			ops.write(jsonString.getBytes(StandardCharsets.UTF_8));
+			ops.close();
+
+			//step 2: test get Flask response (Passed)
+			InputStream ips = new BufferedInputStream(httpURLConnection.getInputStream());
+			BufferedReader reader = new BufferedReader(new InputStreamReader(ips));
+			StringBuilder response= new StringBuilder();
+			String responseLine = null;
+			while ((responseLine = reader.readLine()) != null) {
+				response.append(responseLine);
+			}
+			System.out.println(response.toString());
+		}
+		catch (MalformedURLException e1) {
+			e1.printStackTrace();
+			System.err.println("Malformed URL Exception: check URL");
+		}
+		catch (IOException e2) {
+			e2.printStackTrace();
+			System.err.println("Post Error: Host Exception");
+		}
+
+	}
+
 	private void sendDataToFlaskWMA(int targetCount, List<Integer> trackScore) {
 		try {
-			URL flaskUrl = new URL("http://127.0.0.1.5000/suggestnextmeal");
+			URL flaskUrl = new URL("http://127.0.0.1:5000/suggestnextmeal");
 			HttpURLConnection httpURLConnection = (HttpURLConnection) flaskUrl.openConnection();
 			httpURLConnection.setConnectTimeout(10000);
 			httpURLConnection.setReadTimeout(10000);
 			httpURLConnection.setRequestMethod("POST");
-			httpURLConnection.setRequestProperty("Content-Type", "application/json: utf-8");
+			httpURLConnection.setRequestProperty("Content-Type", "application/json: charset=UTF-8");
 			httpURLConnection.setRequestProperty("Accept", "application/json");
 			httpURLConnection.setDoOutput(true);
 
+			InputStream status = httpURLConnection.getErrorStream();
+			System.out.println(status);
+
+			//JSON not being received by Flask
 			ObjectMapper jsonMapper = new ObjectMapper();
 			ObjectNode jsonData = jsonMapper.createObjectNode();
-			jsonData.put("targetCount", 3);
+
+			jsonData.put("targetCount", jsonMapper.writeValueAsString(targetCount));
 			jsonData.put("trackScore", jsonMapper.writeValueAsString(trackScore));
 
 			String jsonString = jsonMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonData);
@@ -131,6 +188,7 @@ public class MealEntryController {
 			OutputStream ops = httpURLConnection.getOutputStream();
 			ops.write(jsonString.getBytes(StandardCharsets.UTF_8));
 			ops.close();
+			//----------
 
 			InputStream ips = new BufferedInputStream(httpURLConnection.getInputStream());
 			BufferedReader reader = new BufferedReader(new InputStreamReader(ips));
