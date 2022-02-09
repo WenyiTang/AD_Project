f1e75fd5d99bea20e50c83a1955b310e6d67d919
Daniel <danielchua93@gmail.com>
2022-02-09 23:25:24 +0800

    managed to get blog entries from API and use it to set state of react Feed component

--- src/main/resources/static/blog/javascript/react/feed/Feed.js
+++ src/main/resources/static/blog/javascript/react/feed/Feed.js
@@ -3,16 +3,37 @@ class Feed extends React.Component {
         blogEntries: [],
         pageLength: 10,
         pageNo: 0,
+        activeUserId: this.props.userid,
 
     };
+    // Method below kind of "initializes the component",
+    // i.e. it runs when the page is first loaded I think
+    componentDidMount() {
+        fetch (
+            "http://localhost:8080/api/feed/page?" +
+            new URLSearchParams({
+                activeUserId: this.state.activeUserId,
+                pageNo: this.state.pageNo,
+                pageLength: this.state.pageLength
+            }), {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            }).then ( res =>res.json()
+            ).then (
+                data => this.setState({blogEntries: data})
+            );
+    
 
+    }
+    
 
     render() {
     return <>
-        <div>This is your feed</div>
+        <div>Active user id = {this.state.activeUserId}. This is your feed</div>
         <BlogEntry/>
         </>;
     }
 }
-
 
