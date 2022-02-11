35ee44de3e567218bf740959d60afc76a0c4b33c
Daniel <danielchua93@gmail.com>
2022-02-12 04:55:55 +0800

    implemented and tested ImageAPI method for downloading images using postman

--- src/main/java/com/example/adproject/api/ImageAPI.java
+++ src/main/java/com/example/adproject/api/ImageAPI.java
@@ -6,17 +6,28 @@ import java.io.InputStream;
 import org.apache.commons.io.IOUtils;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
-@RequestMapping("/image")
+@RequestMapping("api/image")
 public class ImageAPI {
 
-    @GetMapping( value = "/get",produces = MediaType.IMAGE_JPEG_VALUE)
-    public @ResponseBody byte[] getImageWithMediaType() throws IOException {
+    @GetMapping( value = "/demo",produces = MediaType.IMAGE_JPEG_VALUE, params = {})
+    public @ResponseBody byte[] getImageWithMediaTypeDemo() throws IOException {
         InputStream in = getClass()
                         .getResourceAsStream("/static/blog/images/banmian.jpeg");
+                        return IOUtils.toByteArray(in);
+    }
+
+    @GetMapping( value = "/get",produces = MediaType.IMAGE_JPEG_VALUE, params = {"imagePath"})
+    public @ResponseBody byte[] getImageWithMediaType(@RequestParam String imagePath) throws IOException {
+        String folderPath = "/static";
+        imagePath = folderPath + imagePath;
+        InputStream in = getClass()
+                        .getResourceAsStream(imagePath);
                         return IOUtils.toByteArray(in);
     }
     
