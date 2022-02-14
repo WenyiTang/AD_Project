37babe7e2bc8b118a38c22bbf914b7b9b1640600
glenna <glennatjt@gmail.com>
2022-02-14 16:08:32 +0800

    edit HomeController

--- src/main/java/com/example/adproject/controller/HomeController.java
+++ src/main/java/com/example/adproject/controller/HomeController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -42,34 +43,43 @@ public class HomeController {
 
 
     
-    @GetMapping("/getCurrentGoal")
-    public String currentGoal() {
-    	Integer userId = 3;
+//    @GetMapping("/getCurrentGoal")
+//    public String currentGoal() {
+//    	Integer userId = 3;
+//    	String currentGoal =gRepo.findCurrentGoal(userId).getGoalDescription();
+//		return currentGoal;
+//    	
+//    }
+   
+    @GetMapping("/getCurrentGoal/{userId}")
+    public String currentGoal(@PathVariable Integer userId) {
     	String currentGoal =gRepo.findCurrentGoal(userId).getGoalDescription();
 		return currentGoal;
     	
     }
     
-//    @GetMapping("/getTrack")
-//    public float[] getMealTrack() {
-//    	Integer userId = 3;
+    
+//  @GetMapping("/getTrack")
+//  public ResultJson getMealTrack() {
+//  	Integer userId = 3;
 //		float countOnT = mRepo.findEntryByAuthor(userId).stream()
 //				.filter(x->x.getTrackScore()==1)
 //				.count();
 //		float countOffT= mRepo.findEntryByAuthor(userId).stream()
 //				.filter(x->x.getTrackScore() == 0)
 //				.count();
-//		float[] mealTrackList = new float[2];
-//		mealTrackList[0]=countOnT;
+//
+//		ResultJson r = new ResultJson();
+//		mealTrackList[0] = countOnT;
 //		mealTrackList[1] = countOffT;
-//		return mealTrackList;
-//    	
-//    }
-    
-    
-  @GetMapping("/getTrack")
-  public ResultJson getMealTrack() {
-  	Integer userId = 3;
+//		r.setMealTrack(mealTrackList);
+//		return r;
+//		
+//  }
+  
+  @GetMapping("/getTrack/{userId}")
+  public ResultJson getMealTrack(@PathVariable Integer userId) {
+  	//Integer userId = 3;
 		float countOnT = mRepo.findEntryByAuthor(userId).stream()
 				.filter(x->x.getTrackScore()==1)
 				.count();
@@ -82,7 +92,6 @@ public class HomeController {
 		mealTrackList[1] = countOffT;
 		r.setMealTrack(mealTrackList);
 		return r;
-  	
   }
     
 
