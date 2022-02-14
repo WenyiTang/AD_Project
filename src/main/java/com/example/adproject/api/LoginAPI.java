0624a373dbf2d863567cf8154cef4393f3181547
CY <69207419+kcy-23@users.noreply.github.com>
2022-02-14 17:41:33 +0800

    Added reset email and create users methods to LoginAPI
    - Added additional attribute to User model to store android imageURL path

--- src/main/java/com/example/adproject/api/LoginAPI.java
+++ src/main/java/com/example/adproject/api/LoginAPI.java
@@ -1,11 +1,29 @@
 package com.example.adproject.api;
 
 import com.example.adproject.helper.UserHelper;
+import com.example.adproject.model.Role;
 import com.example.adproject.model.User;
+import com.example.adproject.repo.RoleRepo;
+import com.example.adproject.security.Utility;
+import com.example.adproject.service.UserNotFoundException;
 import com.example.adproject.service.UserService;
+import net.bytebuddy.utility.RandomString;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,7 +32,13 @@ import java.util.Map;
 public class LoginAPI {
 
     @Autowired
+    private JavaMailSender mailSender;
+
+    @Autowired
     private UserService uService;
+
+    @Autowired
+    private RoleRepo rRepo;
 
     @PostMapping("/auth")
     public UserHelper authenticateLogin(@RequestParam String username, @RequestParam String password) {
@@ -27,5 +51,131 @@ public class LoginAPI {
         } else {
             return null;
         }
+    }
+
+    @PostMapping("/reset_email")
+    public Map<String, Integer> sendResetEmail(@RequestParam String email, HttpServletRequest request) {
+        Map<String, Integer> response = new HashMap<>();
+
+        User user = uService.findUserByEmail(email);
+        String token = RandomString.make(30);
+
+        if (user == null) {
+            response.put("status", 1);
+        } else {
+            try {
+                uService.updateResetPasswordToken(token, email);
+                String resetPasswordLink = Utility.getSiteURL(request) + "/reset_password?token=" + token;
+                sendEmail(email, resetPasswordLink);
+                response.put("status", 0);
+            } catch (UserNotFoundException e) {
+                response.put("status", 1);
+            } catch (UnsupportedEncodingException | MessagingException e) {
+                response.put("status", 1);
+            }
+        }
+        return response;
+    }
+
+    @PostMapping("/create01")
+    public Map<String, String> validateAndSetCredentials(@RequestParam MultipartFile multipartFile,
+                                                         @RequestParam String fileName,
+                                                         @RequestParam String imageURL,
+                                                         @RequestParam String username,
+                                                         @RequestParam String password,
+                                                         @RequestParam String email) {
+
+        Map<String, String> response = new HashMap<>();
+
+        if (multipartFile == null || multipartFile.isEmpty()) {
+            response.put("error", "MultipartFileFailure");
+            return response;
+        }
+
+        User u1 = uService.findUserByUsername(username);
+        if (u1 != null) {
+            response.put("status", "Username error");
+            response.put("username", "");
+            return response;
+        }
+
+        User u2 = uService.findUserByEmail(email);
+
+        if (u2 != null) {
+            response.put("status", "Email error");
+            response.put("username", "");
+            return response;
+        }
+
+        if (u1 == null && u2 == null) {
+
+            User newUser = new User();
+            newUser.setUsername(username);
+            newUser.setEmail(email);
+            newUser.setPassword(password);
+            newUser.setProfilePic(fileName);
+            newUser.setImageURL(imageURL);
+            newUser.setEnabled(true);
+
+            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+            String encoderPassword = encoder.encode(newUser.getPassword());
+            newUser.setPassword(encoderPassword);
+
+            Role role = rRepo.findByType("ADMIN");
+            newUser.getRoles().add(role);
+
+            uService.save(newUser);
+
+            String uploadDir = "./images/" + newUser.getId();
+
+            Path uploadPath = Paths.get(uploadDir);
+
+            // Saving user's profile pic into directory
+            if (!Files.exists(uploadPath)) {
+                try {
+                    Files.createDirectories(uploadPath);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            try {
+                File profileImg = new File("images/" + newUser.getId() + "/" + fileName);
+                profileImg.createNewFile();
+                FileOutputStream fout = new FileOutputStream(profileImg);
+                fout.write(multipartFile.getBytes());
+                fout.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            response.put("status", "OK");
+            response.put("username", username);
+        }
+        return response;
+    }
+
+    private void sendEmail(String user_email, String resetPasswordLink)
+            throws MessagingException, UnsupportedEncodingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+
+        helper.setFrom("contact@fooddiary.com", "Food Diary App Support");
+        helper.setTo(user_email);
+
+        String subject = "Reset your password";
+
+        String content = "<p>Hi there,</p> "
+                +"<p>We've received your request to reset your password.</p>"
+                + "<p>Please click on the link below to reset your password:</p>"
+                + "<p><a href=\"" + resetPasswordLink + "\">Reset Password here</a></p>" + "<br>"
+                + "<p>Please ignore this email if you did not make this request.</p>";
+
+        helper.setSubject(subject);
+
+        helper.setText(content, true);
+
+        mailSender.send(message);
+
     }
 }
