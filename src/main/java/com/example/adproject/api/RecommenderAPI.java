c388c27d03093dacd084d08056ad4967e701aaba
wenyi <wenyi1717@gmail.com>
2022-02-15 22:37:19 +0800

    streamline passing of data to android/flask, include userid

--- src/main/java/com/example/adproject/api/RecommenderAPI.java
+++ src/main/java/com/example/adproject/api/RecommenderAPI.java
@@ -1,22 +1,18 @@
 package com.example.adproject.api;
 
 import java.util.ArrayList;
+
 import java.util.List;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.example.adproject.helper.EntriesDataTable;
-import com.example.adproject.helper.Query;
 import com.example.adproject.helper.RecResultJson;
 import com.example.adproject.helper.SearchResult;
 import com.example.adproject.model.MealEntry;
@@ -28,7 +24,7 @@ import com.example.adproject.service.UserService;
 @RestController
 @RequestMapping("/api/recommend")
 public class RecommenderAPI {
-	private String[] titles = new String[5];
+	//private String[] titles = new String[5];
 	
 	@Autowired
 	UserService uService;
@@ -39,9 +35,7 @@ public class RecommenderAPI {
 	@Autowired
 	MealEntryService mService;
 
-	SearchResult results = new SearchResult();
-
-	Query query = new Query();
+//	SearchResult results = new SearchResult();
 
 //	@GetMapping("/getStringData")
 //	public String getString() {
@@ -49,60 +43,101 @@ public class RecommenderAPI {
 //	}
 
 
-	@RequestMapping(value = "/postStringData", method = RequestMethod.POST)
-	public Query postString(@RequestParam String input, String feeling, String track) {
-		System.out.println(input);
-		System.out.println(feeling);
-		System.out.println(track);
-		String q = input + " " + feeling + " " + track;
-		System.out.println(q);
-		query.setInput(q);
-		query.setFeeling(feeling);
-		query.setTrack(track);
-		return query;
-	}
-
-//	@RequestMapping(value = "/postpostStringData", method = RequestMethod.POST)
-//	public String postpostString(@RequestBody String input) {
+//	@RequestMapping(value = "/postStringData/{uid}", method = RequestMethod.POST)
+//	public SearchQuery postString(@PathVariable("uid") Integer userId, @RequestParam String input, String feeling, String track) {
+//		System.out.println("post string: " + userId);
 //		System.out.println(input);
-//		return input;
+//		System.out.println(feeling);
+//		System.out.println(track);
+////		String q = input + " " + track + " " + feeling;
+////		System.out.println(q);
+//		User user = uService.findUser(userId);
+//		SearchQuery query = sqrepo.findSearchQueryByUser(user);
+//		if (query == null) {
+//			query = new SearchQuery(user);
+//		}
+//		query.setQueryHelper(new QueryHelper(input, feeling, track)); 
+//		sqrepo.save(query);
+//		return query;
 //	}
-
-	@GetMapping("/enterquery1")
-	public Query queryString() {
-		return query;
-	}
-
-	@RequestMapping(value = "/runSearch", method = RequestMethod.POST)
-	public SearchResult runSearch() {
-		System.out.println("runSearch1");
-		String url = "http://localhost:5000/enterquery";
+//
+////	@RequestMapping(value = "/postpostStringData", method = RequestMethod.POST)
+////	public String postpostString(@RequestBody String input) {
+////		System.out.println(input);
+////		return input;
+////	}
+//
+//	@GetMapping("/enterquery1/{uid}")
+//	public QueryHelper queryString(@PathVariable("uid") Integer userId) {
+//		User user = uService.findUser(userId);
+//		SearchQuery query = sqrepo.findSearchQueryByUser(user);
+//		return query.getQueryHelper();
+//	}
+//
+//	@RequestMapping(value = "/runSearch", method = RequestMethod.POST)
+//	public SearchResult runSearch(Integer userId) {
+//		System.out.print("post string: " + userId);
+//		System.out.println("runSearch1");
+//		String url = "http://localhost:5000/enterquery?uid=" + userId;
+//		RestTemplate restTemplate = new RestTemplate();
+//		results = restTemplate.getForObject(url, SearchResult.class);
+//		System.out.println("got results from flask");
+//		System.out.println(results.getRes0());
+//		System.out.println(results.getRes1());
+//		System.out.println(results.getRes2());
+//		System.out.println(results.getRes3());
+//		System.out.println(results.getRes4());
+//		results.setResults(new Integer[] {
+//				results.getRes0(), results.getRes1(), results.getRes2(), results.getRes3(), results.getRes4()
+//				});
+//		System.out.println(results.getGoodResult());
+//		return results;
+//	}
+//
+//	@GetMapping("/getResultJson/{uid}")
+//	public RecResultJson resultJson(@PathVariable("uid") Integer userId) {
+//		System.out.print("post string: " + userId);
+//		results = runSearch(userId);
+//		RecResultJson r = new RecResultJson();
+//		System.out.println("getResultJson");
+//		System.out.println(results.getRes0());
+//		System.out.println(results.getRes1());
+//		System.out.println(results.getRes2());
+//		System.out.println(results.getRes3());
+//		System.out.println(results.getRes4());
+//		Integer[] resultArray = results.getResults();
+//		for (int x=0; x<resultArray.length; x++) {
+//			titles[x] = mrepo.findById(resultArray[x]).get().getTitle();
+//		}
+//		System.out.println("In getResultJson");
+//		for (String s : titles) {
+//			System.out.println(s);
+//		}
+//		r.setTitles(titles);
+//		r.setGoodResult(results.getGoodResult()); 
+//		return r;
+//	}
+	
+	@RequestMapping(value = "/postStringData/{uid}/{input}/{feeling}/{track}", method = {RequestMethod.POST, RequestMethod.GET})
+	public RecResultJson postQuery(@PathVariable("uid") Integer userId, @PathVariable("input") String input,
+			@PathVariable("feeling") String feeling, @PathVariable("track") String track) {
+		String url = "http://localhost:5000/enterquery/" 
+			+ userId + "/" + input + "/" + feeling + "/" + track;
 		RestTemplate restTemplate = new RestTemplate();
+		SearchResult results = new SearchResult();
 		results = restTemplate.getForObject(url, SearchResult.class);
-		System.out.println("got results from flask");
-		System.out.println(results.getRes0());
-		System.out.println(results.getRes1());
-		System.out.println(results.getRes2());
-		System.out.println(results.getRes3());
-		System.out.println(results.getRes4());
-		results.setResults(new Integer[] {
-				results.getRes0(), results.getRes1(), results.getRes2(), results.getRes3(), results.getRes4()
-				});
-		System.out.println(results.getGoodResult());
-		return results;
-	}
-
-	@GetMapping("/getResultJson")
-	public RecResultJson resultJson() {
-		results = runSearch();
-		RecResultJson r = new RecResultJson();
 		System.out.println("getResultJson");
 		System.out.println(results.getRes0());
 		System.out.println(results.getRes1());
 		System.out.println(results.getRes2());
 		System.out.println(results.getRes3());
 		System.out.println(results.getRes4());
-		Integer[] resultArray = results.getResults();
+		RecResultJson r = new RecResultJson();
+		Integer[] resultArray = new Integer[] {
+				results.getRes0(), results.getRes1(), results.getRes2(), results.getRes3(), results.getRes4()
+				};
+		
+		String[] titles = new String[resultArray.length];
 		for (int x=0; x<resultArray.length; x++) {
 			titles[x] = mrepo.findById(resultArray[x]).get().getTitle();
 		}
@@ -116,9 +151,10 @@ public class RecommenderAPI {
 	}
 
 	//pass mealEntryTable to Flask
-	@RequestMapping(value = "/passData", method = {RequestMethod.POST, RequestMethod.GET})
-	public EntriesDataTable passDataToFlask() {
-		User user = uService.findUser(1);
+	@RequestMapping(value = "/passData/{uid}", method = {RequestMethod.POST, RequestMethod.GET})
+	public EntriesDataTable passDataToFlask(@PathVariable("uid") String userId) {
+		System.out.println("userId: " + userId);
+		User user = uService.findUser(Integer.parseInt(userId));
 		List<MealEntry> entries = mService.findMealEntryByUser(user);
 		//to implement include friend's posts
 		//...
