f7f5944763e47c6d1ca3e9d2f08679970e8c7f81
Daniel <danielchua93@gmail.com>
2022-02-16 07:42:13 +0800

    added api method for submitting comments

--- src/main/java/com/example/adproject/api/CommentAPI.java
+++ src/main/java/com/example/adproject/api/CommentAPI.java
@@ -5,10 +5,16 @@ import java.util.List;
 
 import com.example.adproject.helper.CommentHelper;
 import com.example.adproject.model.Comment;
+import com.example.adproject.model.MealEntry;
+import com.example.adproject.model.User;
+import com.example.adproject.repo.CommentRepo;
+import com.example.adproject.repo.MealEntryRepo;
+import com.example.adproject.repo.UserRepo;
 import com.example.adproject.service.CommentService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +25,15 @@ public class CommentAPI {
 
     @Autowired
     private CommentService cService;
+
+    @Autowired
+    private MealEntryRepo mRepo;
+
+    @Autowired
+    private UserRepo uRepo;
+
+    @Autowired
+    private CommentRepo cRepo;
 
     @GetMapping("/get")
     public ArrayList<CommentHelper> getComments(@RequestParam Integer mealEntryId) {
@@ -36,6 +51,29 @@ public class CommentAPI {
 
         return commentHelpers;
 
+    }
+    @PostMapping("/submit")
+    public CommentHelper submitComment(@RequestParam Integer mealEntryId,
+                                @RequestParam Integer activeUserId,
+                                @RequestParam String caption) {
+
+            MealEntry mealEntry = mRepo.findById(mealEntryId).get();
+            User activeUser = uRepo.findById(activeUserId).get();
+            
+            if(mealEntry == null || activeUser == null) {
+                return null;
+            }
+
+            Comment comment = new Comment(caption,activeUser,mealEntry);
+
+            cRepo.saveAndFlush(comment);
+
+
+
+            return new CommentHelper(comment);
+            
+            
+        
     }
 
 
